df9299eb82a0f72730b3dbbb2d1feebe8eb431fb
Maulik Vora <maulikvinodbhaivora@mauliks-macbook-air.local>
2018-07-24 11:41:36 +0530

    second

---
+++ BAPS Live/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BAPS Live
+//
+//  Created by Maulik Vinodbhai Vora on 24/07/18.
+//  Copyright © 2018 Maulik Vora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
